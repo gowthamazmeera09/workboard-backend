a4534a005634bd6f996048bf8e88ea0b1cabeb88
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-11-03 22:06:17 +0530

    jfhufhfhududf

--- controller/userController.js
+++ controller/userController.js
@@ -19,7 +19,7 @@ const upload = multer({ storage });
 
 const userRegister = async(req, res) => {
     const { username, email, password, phonenumber } = req.body;
-    const photo = req.file ? req.file.buffer.toString('base64') : null; // Convert buffer to Base64 string
+    const photo = req.file ? req.file.originalname : null; // Convert buffer to Base64 string
 
     try {
         const userEmail = await User.findOne({ email });
@@ -109,10 +109,10 @@ const userLogin = async(req, res) => {
         }
         const token = jwt.sign({ userId: user._id }, secretkey, { expiresIn: "1h" });
         
-        // Sending Base64 encoded photo directly
-        const photo = user.photo ? `data:image/jpeg;base64,${user.photo}` : '';
+        // // Sending Base64 encoded photo directly
+        // const photo = user.photo ? `data:image/jpeg;base64,${user.photo}` : '';
 
-        res.status(200).json({ success: "Login successful", token, userId: user._id, photo });
+        res.status(200).json({ success: "Login successful", token, userId: user._id,photo: user.photo  });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal server error" });
