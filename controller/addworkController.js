e3db9f62f2a788de8f0ff8cbd8b25974d18314e8
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-11-22 00:13:54 +0530

    jicvfvhubfhvshvbsvi0asu8wigr

--- controller/addworkController.js
+++ controller/addworkController.js
@@ -5,15 +5,28 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const storage = multer.memoryStorage({
-
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4() + '-' + Date.now() + path.extname(file.originalname));
+    }
 });
 
+const fileFilter = (req, file, cb) => {
+    const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    if (allowedFileTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
 
-const upload = multer({ storage })
+const upload = multer({ storage, fileFilter })
 
 const workadding = async(req, res)=>{
     const {role,experience,location,standard,subject,vehicletype,paintertype,weldingtype,marbultype} = req.body;
-    const photos = req.files.map(file => file.filename); 
+    const photos = req.files.map(file => file.buffer);
 
     try {
         const user = await User.findById(req.userId);
