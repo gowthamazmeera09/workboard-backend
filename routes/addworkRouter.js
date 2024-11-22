17049440e05d765a9267984362bfbba3b06d9e84
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-11-22 18:34:35 +0530

    jvhufsbhuvbvhubyshsf

--- routes/addworkRouter.js
+++ routes/addworkRouter.js
@@ -1,13 +1,9 @@
-const express = require('express');
 const addworkController = require('../controller/addworkController');
+const express = require('express');
 const VerifyToken = require('../middleware/VerifyToken');
 
 const router = express.Router();
-
-// Add work route
 router.post('/workadding/:userId', VerifyToken, addworkController.workadding);
-
-// Delete work route
 router.delete('/deletework/:workId', addworkController.workdelete);
 
 module.exports = router;
