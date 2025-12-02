f96480cfe9eb26a877d848304c709e4f943cc921
gowthamazmeera09 <gowthamazmeera@gmail.com>
2025-12-02 14:40:25 +0530

    kkkkkkkk

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ dotEnv.config();
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',  // allow all origins (or replace * with your local frontend URL)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyparser.json());
