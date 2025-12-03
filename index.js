1fa6f430c325dbd7809e4d9a64d84ce3f97bc340
gowthamazmeera09 <gowthamazmeera@gmail.com>
2025-12-03 11:49:48 +0530

    gkgkgkgkgkg

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ dotEnv.config();
 
 
 const app = express();
-app.use(cors({
-  origin: '*',  // allow all origins (or replace * with your local frontend URL)
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyparser.json());
