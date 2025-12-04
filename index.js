5841b60d909363b0e11075b781016bb988bf7812
gowthamazmeera09 <gowthamazmeera@gmail.com>
2025-12-04 11:54:17 +0530

    kjhgghjk

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ dotEnv.config();
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
@@ -29,6 +32,7 @@ mongoose.connect(process.env.MONGO_URL)
 })
 app.use('/user',userRouter);
 app.use('/work',addworkRouter);
+
 
 
 
